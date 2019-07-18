c4f005eaa6724a9c207b860fff6105a3caaa8e3a
鄭竹容 <zhengzhurong@192-168-1-120.tpgi.com.au>
2019-07-18 14:35:27 +1000

    Updates

---
+++ precache-manifest.b38f2553070e61767d6fb13f8a04c9ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7260f73b5cc7085ab044342675125aef",
+    "url": "/The-F2E-2019-No1-Tomato-Clock/index.html"
+  },
+  {
+    "revision": "84c408462754665778dd",
+    "url": "/The-F2E-2019-No1-Tomato-Clock/static/css/2.1f156bdb.chunk.css"
+  },
+  {
+    "revision": "fdef437205e52428c22e",
+    "url": "/The-F2E-2019-No1-Tomato-Clock/static/css/main.c3ca22e5.chunk.css"
+  },
+  {
+    "revision": "84c408462754665778dd",
+    "url": "/The-F2E-2019-No1-Tomato-Clock/static/js/2.a62ae5f0.chunk.js"
+  },
+  {
+    "revision": "fdef437205e52428c22e",
+    "url": "/The-F2E-2019-No1-Tomato-Clock/static/js/main.fde65aa4.chunk.js"
+  },
+  {
+    "revision": "f13bb05a669b5c8ddfca",
+    "url": "/The-F2E-2019-No1-Tomato-Clock/static/js/runtime~main.982d955e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d3aa2183f808cbb44429de5cb2a0c57d.js"
+  "/The-F2E-2019-No1-Tomato-Clock/precache-manifest.b38f2553070e61767d6fb13f8a04c9ae.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/The-F2E-2019-No1-Tomato-Clock/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
